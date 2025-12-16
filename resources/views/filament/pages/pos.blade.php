64a9a211cc1547c5651c070b402e74b968d0af2b
Evan Helga <evanabaddon@gmail.com>
2025-12-16 21:50:45 +0700

    Update pos.blade.php

--- resources/views/filament/pages/pos.blade.php
+++ resources/views/filament/pages/pos.blade.php
@@ -516,7 +516,7 @@
                         title="Cetak ulang order ke dapur">
                         <svg wire:loading.remove wire:target="reprintOrder" class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path></svg>
                         <svg wire:loading wire:target="reprintOrder" class="w-4 h-4 animate-spin" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path></svg>
-                        Cetak Dapur
+                        Cetak Order
                     </button>
                 </div>
 
