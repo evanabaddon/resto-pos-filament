6e58540804b989b9341434d43894557ea319cac0
Evan Helga <evanabaddon@gmail.com>
2026-01-21 12:25:47 +0700

    Update CustomerDisplayController.php

--- app/Http/Controllers/CustomerDisplayController.php
+++ app/Http/Controllers/CustomerDisplayController.php
@@ -11,16 +11,12 @@ class CustomerDisplayController extends Controller
     {
         $settings = app(GeneralSettings::class);
 
-        // Try to get landing page settings with proper error handling
-        try {
-            $landingSettings = app(LandingPageSettings::class);
-        } catch (\Exception $e) {
-            // If settings don't exist, create stdClass with default colors
-            $landingSettings = (object) [
-                'primary_color' => '#667eea',
-                'secondary_color' => '#764ba2'
-            ];
-        }
+        // Always use default colors - don't try to load LandingPageSettings
+        // This prevents any MissingSettings errors
+        $landingSettings = (object) [
+            'primary_color' => '#667eea',
+            'secondary_color' => '#764ba2'
+        ];
 
         return view('customer-display', [
             'settings' => $settings,
